9bb0aa9a74d1a48a64e3612fe391988fc0ba7835
Jep <xjn377969949@live.com>
2016-12-01 17:57:42 +0800

    Strip 2016_12_01

---
+++ STrip/HotelTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  HotelTableViewCell.swift
+//  STrip
+//
+//  Created by Jep Xia on 16/11/29.
+//  Copyright © 2016年 Jep Xia. All rights reserved.
+//
+
+import UIKit
+
+class HotelTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var hotelImage: UIImageView!
+    @IBOutlet weak var hotelName: UILabel!
+    @IBOutlet weak var hotelScore: UILabel!
+    @IBOutlet weak var hotelLocation: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ STrip/HotelViewController.swift
@@ -0,0 +1,54 @@
+//
+//  HotelViewController.swift
+//  STrip
+//
+//  Created by Jep Xia on 16/11/29.
+//  Copyright © 2016年 Jep Xia. All rights reserved.
+//
+
+import UIKit
+
+class HotelViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initTableView()
+        // Do any additional setup after loading the view.
+    }
+
+    func initTableView () {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return cell
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
